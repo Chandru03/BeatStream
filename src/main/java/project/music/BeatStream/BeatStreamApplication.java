9d7af2f4e3ff9241238685a5f102dbd14004c00a
Chandru03 <tamilchandru03@gmail.com>
2024-02-01 23:58:32 +0530

    First Commit

---
+++ src/main/java/project/music/BeatStream/BeatStreamApplication.java
@@ -0,0 +1,13 @@
+package project.music.BeatStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/music/BeatStream/BeatStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package project.music.BeatStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
